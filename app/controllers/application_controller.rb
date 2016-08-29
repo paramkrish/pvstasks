d00e1bcca9205187f1998e485ddd98e67d9406a6
Param Krish <paramkrish@apples-MacBook-Pro.local>
2016-08-29 08:30:54 +0530

    ckeditor removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
-	  
-=begin	  unless Rails.application.config.consider_all_requests_local
-		rescue_from ActiveRecord::RecordNotFound, with: -> { render_404 } 
-		rescue_from ActionController::UnknownController, with: -> { render_404  }
-	    rescue_from ActionController::RoutingError, with: -> { render_404  }
-		end
 
-		protected
-
-		def render_404
-		  render file: 'public/404.html', status: :not_found
-		end
-=end
- end
+end
