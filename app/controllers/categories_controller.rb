5f5c449294626dbd8f9d02a4b5bdbeb9f23da58e
Param Krish <paramkrish@apples-MacBook-Pro.local>
2016-09-25 10:45:21 +0530

    updates

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -39,6 +39,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @taskCount = gettasks_count(params[:id])
+    if (session[:current_user_id])
+     @current_user = User.find(session[:current_user_id])
+    end
+    
   end
 
   def update
